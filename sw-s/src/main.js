0d38a66d3213cf4bab5fee9a598e37b3ba4eb944
ErYueYuan <63904903+ErYueYuan@users.noreply.github.com>
2021-10-14 20:27:46 +0800

    1
    1

--- sw-s/src/main.js
+++ sw-s/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vueEsign from 'vue-esign'
+Vue.use(vueEsign)
 Vue.config.productionTip = false
 
 new Vue({
--- sw-s/src/views/Home.vue
+++ sw-s/src/views/Home.vue
@@ -2,29 +2,78 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+     <div class="box" style="border: 2px solid #E6E6E6 ;background-color: red;">
+    <div >
+    <vue-esign
+      ref="esign"
+      :width="800"
+      :height="300"
+      :isCrop="isCrop"
+      :lineWidth="lineWidth"
+      :lineColor="lineColor"
+      :bgColor.sync="bgColor"
+    />  
+    <button @click="handleReset">清空画板</button>
+    <button @click="handleGenerate">生成图片</button>
+    </div>
+     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import { mapActions } from 'vuex'
+import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  name: 'Home',
-  computed: {
-   
+  name: "Home",
+  computed: {},
+  data() {
+    return {
+      lineWidth: 6,
+      lineColor: "#000000",
+      bgColor: "",
+      resultImg: "",
+      isCrop: false,
+    };
   },
   components: {
     HelloWorld,
   },
-  created() {
-
-  },
+  created() {},
   mounted() {
-    console.log(this.$store.state.user)
+    console.log(this.$store.state.user);
+    this.$nextTick(()=>{
+     let ctrx = this.$refs.esign.$el;
+     let context = ctrx.getContext('2d');
+     context.save();
+    context.translate(400,150);
+     context.rotate(Math.PI);
+     context.scale(1,1);
+      console.log(this.$refs.esign.$el);
+    })
   },
   methods: {
-     ...mapActions(['getData']),
-  },
-}
+    handleReset() {
+       this.$nextTick(()=>{
+      this.$refs.esign.$el
+    })
+      this.$refs.esign.reset();
+    },
+    handleGenerate() {
+      this.$refs.esign
+        .generate
+        .then((res) => {
+          this.resultImg = res;
+        })
+        .catch((err) => {
+          alert(err); // 画布没有签字时会执行这里 'Not Signned'
+        });
+    },
+  }
+};
 </script>
+<style scoped>
+.box {
+  background: #cccccc;
+  transform: rotate(45deg);
+}
+</style>
